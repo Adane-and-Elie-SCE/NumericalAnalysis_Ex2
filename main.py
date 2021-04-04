5e114cc63192743c2b11b89b16761f4ed8422cac
Elie Bracha <eliebrachapro@gmail.com>
2021-04-04 13:45:34 +0200

    Main changes

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 
-import numpy as np
 
 def detCalc(mat):
 
@@ -21,14 +20,62 @@ def detCalc(mat):
     return det
 
 
+def elementary(mat, tag):
+
+    i, j = tag
+    ans = mat(len(mat), 1)
+    ans[i][j] = -1 * (mat[i][j] / mat[j][j])
+    return ans
+
+
+def inverse(mat):
+
+    if detCalc(mat) == 0:
+        print("No inverse!")
+        return None
+
+    temp = mat# Copy of self matrix
+    ans = (len(self), 1)  # unit matrix
+
+    # below diagonal
+    for j in range(0, self.size - 1):
+        for i in range(j + 1, self.size):
+            e = temp.elementary([i, j])
+            temp = e * temp
+            ans = e * ans
+
+     # above diagnol
+    for j in range(self.size - 1, 0, -1):
+        for i in range(j - 1, -1, -1):
+            e = temp.elementary([i, j])
+            temp = e * temp
+            ans = e * ans
+
+     # final step
+    for i in range(len(ans)):
+        p = temp[i][i]
+        for j in range(len(ans)):
+            temp[i][j] /= p
+            ans[i][j] /= p
+
+        return ans
+
+
+def solve(mat, b):
+    if detCalc(mat) == 0:
+        inverse = Inverse(mat)
+    else:
+        inverse = LU(mat)
+
+
+
 A = [[5,2,1,4,6], [9,4,2,5,2], [11,5,7,3,9], [5,6,6,7,2], [7,5,9,3,3]]
 B = [[1,2,3], [4,5,6], [7,8,9]]
 
-x = np.array(A)
 
 
 print(detCalc(A))
-print(int(np.ling.det(x)))
+
 
 
 
